8fc7933172f401e5d9f582f8c7fc0120311c50bf
Mahbub Zulkarnain <prof.bub@live.com>
2022-09-30 00:36:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-IP=$(echo $1 | egrep -o "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
+IP=$(echo "$1" | grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$")
 
-if [ ! $IP ]
-then
-    echo "Need IP"
-    exit 1
+if [ ! "$IP" ]; then
+  echo "Need IP"
+  exit 1
 fi
 
-DAYS=$(echo $2 | egrep -o "[0-9]")
-if [ ! $DAYS ]
-then
-    DAYS=365
+DAYS=$(echo "$2" | grep -E "[0-9]+")
+if [ ! "$DAYS" ]; then
+  DAYS=365
 fi
 
 echo "
@@ -37,8 +35,11 @@ subjectAltName = @alt_names
 
 [alt_names]
 IP.1 = $IP
-" > openssl.cnf
+" >openssl.cnf
 
-openssl req -x509 -nodes -days $(echo DAYS) -newkey rsa:2048 -keyout key.pem -out cert.pem -config openssl.cnf
+openssl req -x509 -nodes -days "${DAYS}" -newkey rsa:2048 -keyout key.pem -out cert.pem -config openssl.cnf
 rm openssl.cnf
 openssl x509 -noout -text -fingerprint -in cert.pem
+
+echo
+echo "Generated cert.pem key.pem | IP ${IP}, for ${DAYS} day."
